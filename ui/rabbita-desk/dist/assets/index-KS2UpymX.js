08557c12e982f6a734ddf5ecf94f7227db476098
giantclaw <16783345+giantclaw@user.noreply.gitee.com>
2026-06-30 01:12:08 +0800

    Add first-class Desk mode

--- ui/rabbita-desk/dist/assets/index-KS2UpymX.js
+++ ui/rabbita-desk/dist/assets/index-KS2UpymX.js
@@ -6,4 +6,4 @@
         <p>Preparing the explorer, previews, inspector, and activity drawer.</p>
       </div>
     </div>
-  `);await E(()=>import("./_rabbita_main-entry-COmFKIhS.js"),[]);
+  `);await E(()=>import("./_rabbita_main-entry-CszVyYBt.js"),[]);
